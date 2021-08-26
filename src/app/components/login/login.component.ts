b83159a5d23e9e46597bb09d4b328275e5529cf3
NicoleAsqui <nasqui@espol.edu.ec>
2021-08-26 03:45:33 -0500

    merge

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  forma:FormGroup;
+
+  constructor(private fb:FormBuilder) {
+
+    this.crearFormulario();
+
+   }
 
   ngOnInit(): void {
   }
+
+
+  crearFormulario(){
+    this.forma=this.fb.group({
+
+      usuario:['',Validators.required],
+      password:['',[Validators.required,Validators.minLength(6)]]
+
+    })
+  }
+
+  get UsuarioNoValido(){
+    return this.forma.get('usuario').invalid && this.forma.get('usuario').touched
+  }
+  get PasswordNoValido(){
+    return this.forma.get('password').invalid && this.forma.get('password').touched
+  }
+
+
+  guardar(){
+  
+    if(this.forma.invalid) {
+      return Object.values(this.forma.controls).forEach(control=>{
+        control.markAsTouched();
+      })
+    }
+    
+    //Posteo de inf, a base de datos, a correo 
+    console.log(this.forma.value);
+    
+    
+    }
+
 
 }
