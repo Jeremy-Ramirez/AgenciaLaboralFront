efc0cdad4e1a80bede06c3313da10b2d1616cd54
NicoleAsqui <nasqui@espol.edu.ec>
2021-10-12 11:04:45 -0500

    cambios

--- src/app/components/empresa/sesion-empresa/sesion-empresa.component.ts
+++ src/app/components/empresa/sesion-empresa/sesion-empresa.component.ts
@@ -16,15 +16,8 @@ export class SesionEmpresaComponent implements OnInit {
     private _empresaService: EmpresaService) { }
 
   ngOnInit(): void {
-<<<<<<< HEAD
     this._empresaService.loginEmpresa().subscribe((resp:any)=>{
       Emitters.authEmitter.emit(true);  
-=======
-    this.http.get('http://localhost:8000/api/userempresa/', {withCredentials: true}).subscribe(
-      (res: any) => {
-        console.log(res)
-        Emitters.authEmitter.emit(true);
->>>>>>> b727e90ced10512aed16a7b7bba1ed95b1e22413
       },
       err => {
         Emitters.authEmitter.emit(false);
