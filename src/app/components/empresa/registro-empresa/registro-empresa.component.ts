cd41b39d3b1dcc9f7b8ffe6e8ae4b2138b6cee10
NicoleAsqui <nasqui@espol.edu.ec>
2021-10-29 01:41:22 -0500

    [#178906064] Validaciones

--- src/app/components/empresa/registro-empresa/registro-empresa.component.ts
+++ src/app/components/empresa/registro-empresa/registro-empresa.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-registro-empresa',
@@ -10,7 +11,8 @@ import {Router} from '@angular/router';
 })
 export class RegistroEmpresaComponent implements OnInit {
   form: FormGroup;
-
+  id='';
+  hide: boolean = true;
   constructor(
     private formBuilder: FormBuilder,
     private http: HttpClient,
@@ -27,8 +29,24 @@ export class RegistroEmpresaComponent implements OnInit {
     this.http.post('https://agencialaboralproyecto.pythonanywhere.com/api/loginempresa/', this.form.getRawValue(), {
       withCredentials: true
     }).subscribe((res: any)=>{
+      console.log(res.jwt)
+      console.log(this.getDecodedAccessToken(res.jwt));
+      this.id=this.getDecodedAccessToken(res.jwt).id;
       this.router.navigate(['/empresa/sesionEmpresa/perfilEmpresa'])
       console.log(res.id)
-    });
+    },err => alert('USUARIO O CONTRASEÑA INCORRECTA'));
+  }
+ 
+  getDecodedAccessToken(token: string): any {
+    try{
+        return jwt_decode(token);
+    }
+    catch(Error){
+        return null;
+    }
+  }
+
+  show() {
+    this.hide = !this.hide;
   }
 }
