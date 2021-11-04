f235b7aa0421a60b104abb02e6f4d8f8859ca218
diacrami <diacrami@espol.edu.ec>
2021-11-04 18:22:32 -0500

    perfil aspirante y cambio en sugerencias

--- src/app/components/representante/sugerencias-comentarios/sugerencias-comentarios.component.ts
+++ src/app/components/representante/sugerencias-comentarios/sugerencias-comentarios.component.ts
@@ -16,12 +16,13 @@ export class SugerenciasComentariosComponent implements OnInit {
   id:'';
   file: any;
   message = '';
+  imagenValida: boolean=true;
   usuarioActual: any;
   miFormulario: FormGroup = this.fb.group({
     titulo: ["", Validators.required],
     descripcion: ["", Validators.required],
     usuario_idusuario: 1,
-    imagen: ["", Validators.required],
+    imagen: ["", [Validators.required, Validators.pattern("^.*\.(jpg|jpeg|png)$")]],
     
 
   })
@@ -37,8 +38,16 @@ export class SugerenciasComentariosComponent implements OnInit {
    
    handleFileInput(event: Event){
 
+    if((<HTMLInputElement>event.target).files[0].size>2000000){
+      this.imagenValida=false;
+      alert('El archivo supera los 6Mb.');
+    }else{
+      this.imagenValida=true;
       this.file=(<HTMLInputElement>event.target).files[0];
       console.log("archivo", this.file)
+    }
+    
+      
 
     
    }
