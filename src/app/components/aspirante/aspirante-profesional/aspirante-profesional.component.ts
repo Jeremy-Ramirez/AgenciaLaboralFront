ddcb86015ca77534dc240501cef496afa46cb4fc
diacrami <diacrami@espol.edu.ec>
2021-12-01 19:53:27 -0500

    [#178905732] Formación Profesional

--- src/app/components/aspirante/aspirante-profesional/aspirante-profesional.component.ts
+++ src/app/components/aspirante/aspirante-profesional/aspirante-profesional.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProfesionesService } from '../../../servicios/profesiones.service';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Emitters } from '../clases/emitters';
+import { FormacionProfesionalService } from '../../../servicios/formacion-profesional.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-aspirante-profesional',
@@ -21,26 +24,45 @@ export class AspiranteProfesionalComponent implements OnInit {
   niveles:any[]=[];
   id: any;
   message = '';
-  constructor(private fb: FormBuilder,private _profesiones:ProfesionesService,private http:HttpClient, private rutaActiva: ActivatedRoute ) { }
+
+  agregarUnaFormacion=true;
+
+  listaFormaciones:any[]=[];
+  idAspirante:any;
+  nombresNiveles:any;
+  suscription: Subscription; 
+  //validaciones
+  fechaCorrectaInicio=true;
+  fechaCorrectaCierre=true;
+  siguiente=false;
+
+  constructor(private fb: FormBuilder,private _profesiones:ProfesionesService,private http:HttpClient, private rutaActiva: ActivatedRoute, 
+    private formacionProfesionalService: FormacionProfesionalService, private router: Router) { }
 
 
  
   miFormulario: FormGroup= this.fb.group({
     
     numerohijos: ["", [Validators.required]],
-    experiencialaboral: ["", [Validators.required]],
-    campolaboral:["",[Validators.required]],
-    experticia:["",[Validators.required]],
+    salarioMinimoAceptado: ["", [Validators.required]],
+    descripcionPerfilProfesional:["",[Validators.required]],
     videopresentacion:["",[Validators.required, Validators.pattern("^.*\.(mp4|mkv|avi)$")]],
     aniosexperiencia:["",[Validators.required]],
     fechanacimiento:["",[Validators.required, ]],
     posibilidadviajar:["",[Validators.required,Validators.maxLength(2),Validators.pattern("(si|no)+")]],
     posibilidadcambioresidencia:["",[Validators.required,Validators.maxLength(2),Validators.pattern("(si|no)+")]],
-    estadoestudios:["",[Validators.required, ]],
     profesiones_idprofesiones:["",[Validators.required]],
     idiomas:["",[Validators.required]],
-    nivelestudios_idnivelestudios:["",[Validators.required, ]],
     usuario_idusuario:null,
+
+    
+    /*nivelestudios_idnivelestudios: ["", [Validators.required]],
+    centroeducativo: ["", [Validators.required]],
+    campolaboral:["",[Validators.required]],
+    estadoestudios:["",[Validators.required]],
+    fechainicio:["",[Validators.required]],
+    fechacierre:["",[Validators.required, ]],
+    aspirante_idaspirante:null,*/
   })
 
 
@@ -49,7 +71,22 @@ export class AspiranteProfesionalComponent implements OnInit {
             && this.miFormulario.controls[campo].touched;
   }
 
+  miFormularioFormacion: FormGroup= this.fb.group({
+    
+    nivelestudios_idnivelestudios: ["", [Validators.required]],
+    centroeducativo: ["", [Validators.required]],
+    campolaboral:["",[Validators.required]],
+    estadoestudios:["",[Validators.required]],
+    areaestudios:["",[Validators.required]],
+    fechainicio:["",[Validators.required]],
+    fechacierre:["",[Validators.required]],
+    aspirante_idaspirante:null,
+  })
 
+  campoEsValido2(campo: string){
+    return this.miFormularioFormacion.controls[campo].errors 
+            && this.miFormularioFormacion.controls[campo].touched;
+  }
 
 
  
@@ -59,12 +96,24 @@ export class AspiranteProfesionalComponent implements OnInit {
         this.id = params.id;
       }
     )*/
+    this.suscription = this.formacionProfesionalService.refresh$.subscribe(()=>{
+      this.getFormacionProfesional();
+    })
 
     this.http.get('https://agencialaboralproyecto.pythonanywhere.com/api/userusuario/', {withCredentials: true}).subscribe(
       (res: any) => {
         this.message = `Hi ${res.idusuario}`;
         this.id=res.idusuario
+        for(let asp of this.aspirantes){
+          console.log("aaa")
+          if(asp.usuario_idusuario==res.idusuario){
+            this.idAspirante=asp.idaspirante;
+            console.log("ID DEL ASPIRANTE ACTUAL", this.idAspirante)
+          }
+        }
+        
         Emitters.authEmitter.emit(true);
+        
       },
       err => {
         this.message = 'You are not logged in';
@@ -79,8 +128,15 @@ export class AspiranteProfesionalComponent implements OnInit {
 
     this.getAspirantes();
     this.getNivelesEstudios();
+    this.getFormacionProfesional();
 
   }
+
+  ngOnDestroy():void{
+    this.suscription.unsubscribe();
+    console.log('Observable cerrado');
+  }
+
 
   getAspirantes(){
     this.http.get('https://agencialaboralproyecto.pythonanywhere.com/api/aspirantes/').subscribe((doc:any)=>{
@@ -90,7 +146,6 @@ export class AspiranteProfesionalComponent implements OnInit {
   }
 
 
-  
   getNivelesEstudios(){
     this.http.get('https://agencialaboralproyecto.pythonanywhere.com/api/nivelestudios/').subscribe((nivel:any)=>{
     this.niveles=nivel;
@@ -98,6 +153,17 @@ export class AspiranteProfesionalComponent implements OnInit {
     })
   }
 
+  getFormacionProfesional(){
+    this.formacionProfesionalService.getFormacionProfesional().subscribe(listaFormaciones=>{
+      this.listaFormaciones=listaFormaciones;
+      console.log(this.listaFormaciones)
+    })
+  }
+
+  
+
+  
+  
   handleFileInput(event: Event){
     if((<HTMLInputElement>event.target).files[0].size>6000000){
       this.videoValido=false;
@@ -113,95 +179,209 @@ export class AspiranteProfesionalComponent implements OnInit {
   
  }
 
- guardar(){
 
-  let formData= new FormData();
+  guardar(){
+
+    let formData= new FormData();
     formData.append('numerohijos',this.miFormulario.controls['numerohijos'].value)
-    formData.append('experiencialaboral',this.miFormulario.controls['experiencialaboral'].value)
-    formData.append('campolaboral',this.miFormulario.controls['campolaboral'].value)
-    formData.append('experticia',this.miFormulario.controls['experticia'].value)
+    formData.append('salarioMinimoAceptado',this.miFormulario.controls['salarioMinimoAceptado'].value)
+    formData.append('descripcionPerfilProfesional',this.miFormulario.controls['descripcionPerfilProfesional'].value)
     formData.append('aniosexperiencia',this.miFormulario.controls['aniosexperiencia'].value)
     formData.append('fechanacimiento',this.miFormulario.controls['fechanacimiento'].value)
     formData.append('videopresentacion',this.file)
     formData.append('posibilidadviajar',this.miFormulario.controls['posibilidadviajar'].value)
     formData.append('posibilidadcambioresidencia',this.miFormulario.controls['posibilidadcambioresidencia'].value)
-    formData.append('estadoestudios',this.miFormulario.controls['estadoestudios'].value)
     formData.append('profesiones_idprofesiones',this.miFormulario.controls['profesiones_idprofesiones'].value)
     formData.append('idiomas',this.miFormulario.controls['idiomas'].value)
-    formData.append('nivelestudios_idnivelestudios',this.miFormulario.controls['nivelestudios_idnivelestudios'].value)
     formData.append('usuario_idusuario',this.id)
 
 
-  for(let asp of this.aspirantes){
-    if(asp.usuario_idusuario==this.id){
+    for(let asp of this.aspirantes){
+      if(asp.usuario_idusuario==this.id){
 
-      console.log(asp.idaspirante);
-    this.http.put('https://agencialaboralproyecto.pythonanywhere.com/api/aspirantes/'+ asp.idaspirante, formData).subscribe(
-      resp => console.log(resp),
-      err => console.log(err)
+        console.log(asp.idaspirante);
+        this.http.put('https://agencialaboralproyecto.pythonanywhere.com/api/aspirantes/'+ asp.idaspirante, formData).subscribe(
+          resp => console.log(resp),
+          err => console.log(err)
+    
+        )
+      }
 
-      )
     }
 
+    /*for(let formacion of this.listaFormaciones){
+      this.guardarFormación(formacion)
+    }*/
+    this.siguientePagina()
+
+    
+    
+    
+  
   }
 
-  
-  alert('DATOS PROFESIONALES GUARDADOS');
-  this.miFormulario.reset();
-  
+  guardarFormacion(){
 
-}
+    for(let asp of this.aspirantes){
+      if(asp.usuario_idusuario==this.id){
+        
+        this.miFormularioFormacion.get('aspirante_idaspirante').setValue(asp.idaspirante);
 
-validarFechaNacimiento(){
-    
-  let fechaNacimiento=this.miFormulario.controls['fechanacimiento'].value
-  console.log(fechaNacimiento, new Date().toISOString().split('T')[0])
-  let fechaActual=new Date().toISOString().split('T')[0]  
-  let diaActual= fechaActual.substring(8,10)
-  let mesActual= fechaActual.substring(5,7)
-  let anioActual= fechaActual.substring(0,4)
-  console.log(diaActual)
-  console.log(mesActual)
-  console.log(anioActual)
-  let mesNacimiento= fechaNacimiento.substring(5,7)
-  let diaNacimiento= fechaNacimiento.substring(8,10)
-  let anioNacimiento= fechaNacimiento.substring(0,4)
-  
-  console.log(diaNacimiento)
-  console.log(mesNacimiento)
-  console.log(anioNacimiento)
-  console.log("revisa")
-  if(Number(anioActual)-Number(anioNacimiento)==18 ){
-    console.log("mes resta",Number(mesActual)-Number(mesNacimiento))
-    if(Number(mesActual)-Number(mesNacimiento)<0){
-      console.log("es menor a 18")
-      this.fechaCorrecta=false;
-      console.log("dia resta",Number(diaActual)-Number(diaNacimiento))
-      
+
+        
+        console.log(asp.idaspirante);
+        /*this.http.post('https://agencialaboralproyecto.pythonanywhere.com/api/formacionprofesional/', this.miFormularioFormacion.value).subscribe(
+          resp => console.log(resp),
+          err => console.log(err)
+        )*/
+
+
+        this.formacionProfesionalService.postFormacionProfesional(
+          this.miFormularioFormacion.value).subscribe(data=>{
+            console.log("Datos del post",data)
+            this.miFormularioFormacion.reset();
+          });
+        
+      }
 
     }
-    if(Number(mesActual)-Number(mesNacimiento)==0){
-      if(Number(diaActual)-Number(diaNacimiento)<0){
+    
+    
+  
+  }
+
+
+  
+  
+  validarFechaNacimiento(){
+    
+    let fechaNacimiento=this.miFormulario.controls['fechanacimiento'].value
+    console.log(fechaNacimiento, new Date().toISOString().split('T')[0])
+    let fechaActual=new Date().toISOString().split('T')[0]  
+    let diaActual= fechaActual.substring(8,10)
+    let mesActual= fechaActual.substring(5,7)
+    let anioActual= fechaActual.substring(0,4)
+    console.log(diaActual)
+    console.log(mesActual)
+    console.log(anioActual)
+    let mesNacimiento= fechaNacimiento.substring(5,7)
+    let diaNacimiento= fechaNacimiento.substring(8,10)
+    let anioNacimiento= fechaNacimiento.substring(0,4)
+    
+    console.log(diaNacimiento)
+    console.log(mesNacimiento)
+    console.log(anioNacimiento)
+    console.log("revisa")
+    if(Number(anioActual)-Number(anioNacimiento)==18 ){
+      console.log("mes resta",Number(mesActual)-Number(mesNacimiento))
+      if(Number(mesActual)-Number(mesNacimiento)<0){
         console.log("es menor a 18")
         this.fechaCorrecta=false;
-      }else{
-        this.fechaCorrecta=true;
+        console.log("dia resta",Number(diaActual)-Number(diaNacimiento))
+        
+
       }
+      if(Number(mesActual)-Number(mesNacimiento)==0){
+        if(Number(diaActual)-Number(diaNacimiento)<0){
+          console.log("es menor a 18")
+          this.fechaCorrecta=false;
+        }else{
+          this.fechaCorrecta=true;
+        }
+      }
+    }else if(Number(anioActual)-Number(anioNacimiento)<18 ){
+      console.log("es menor a 18")
+      this.fechaCorrecta=false;
+
+
+    }else{
+      this.fechaCorrecta=true;
     }
-  }else if(Number(anioActual)-Number(anioNacimiento)<18 ){
-    console.log("es menor a 18")
-    this.fechaCorrecta=false;
 
+    /*if(fechaActual>fechaNacimiento){
+      
+      
+      console.log("entra")
+    }*/
 
-  }else{
-    this.fechaCorrecta=true;
   }
 
-  /*if(fechaActual>fechaNacimiento){
-    
-    
-    console.log("entra")
-  }*/
+  nuevaFormacion(){
+    this.agregarUnaFormacion=true;
+  }
+  
+  cerrar(){
+    this.agregarUnaFormacion=false;
+  }
 
-}
+  siguientePagina(){
+    this.siguiente=true;
+  }
+
+  anteriorPagina(){
+    this.siguiente=false;
+  }
+
+  //Agrega una nueva formación a la lista
+
+  agregarUna(){
+    this.agregarUnaFormacion=false;
+    //console.log(this.listaFormaciones)
+    //this.listaFormaciones.push(this.miFormulario.controls)
+    //console.log("formaciones",this.listaFormaciones)
+
+
+
+  }
+
+  finalizar(){
+    alert('DATOS PROFESIONALES GUARDADOS');
+    this.miFormulario.reset();
+    this.miFormularioFormacion.reset();
+    this.router.navigate( [`/aspirante/sesionAspirante/perfilAspirante`]);
+  }
+
+
+  
+
+
+
+  validarFechainicio(){
+    
+    let fechaNacimiento=this.miFormularioFormacion.controls['fechainicio'].value
+    console.log(fechaNacimiento, new Date().toISOString().split('T')[0])
+    let fechaActual=new Date().toISOString().split('T')[0]  
+    
+
+    if(fechaActual<=this.miFormularioFormacion.controls['fechainicio'].value){
+      this.fechaCorrectaInicio=true;
+      
+      console.log("entra")
+    }else{
+      this.fechaCorrectaInicio=false;
+
+    }
+
+  }
+
+
+  validarFechacierre(){
+    
+    
+    let fechaNacimiento=this.miFormularioFormacion.controls['fechacierre'].value
+    console.log(fechaNacimiento, new Date().toISOString().split('T')[0])
+    let fechaActual=new Date().toISOString().split('T')[0]  
+    
+
+    if(fechaActual<=this.miFormularioFormacion.controls['fechacierre'].value){
+      this.fechaCorrectaCierre=true;
+      
+      console.log("entra")
+    }
+    else{
+      this.fechaCorrectaCierre=false;
+
+    }
+
+  }
 }
