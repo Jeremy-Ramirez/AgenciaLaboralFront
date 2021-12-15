19e61aaaa118568519eae1aa2cd69f0c9913fd91
diacrami <diacrami@espol.edu.ec>
2021-12-15 11:56:57 -0500

    [#178905895] Aceptar aspirante y ver su perfil

--- src/app/components/administrador/aspirantes-aceptados/aspirantes-aceptados.component.ts
+++ src/app/components/administrador/aspirantes-aceptados/aspirantes-aceptados.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 import { ActivatedRoute } from '@angular/router';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+import { VistaPerfilAspiranteComponent } from '../vista-perfil-aspirante/vista-perfil-aspirante.component';
 
 @Component({
   selector: 'app-aspirantes-aceptados',
@@ -11,13 +15,41 @@ export class AspirantesAceptadosComponent implements OnInit {
 
   usuarios: any[]=[];
   aspirantes: any[]=[];
-  
+  listausuariosAspirantes: any[]=[];
+  columnas: string[] = ['nombreusuario','nombre', 'apellido','nodocumento', 'correo','boton'];
+  dataSource:any; 
+
   idAspirante: any;
   
-  constructor( private http: HttpClient, private rutaActiva: ActivatedRoute) {
+  constructor( 
+    private http: HttpClient, 
+    private rutaActiva: ActivatedRoute,
+    public dialog: MatDialog
+    ) {
     this.getUsuarios();
     console.log("ASP", this.idAspirante)
   }
+
+  abrirDialogo(aspirante: any) {
+    const dialogo1 = this.dialog.open(VistaPerfilAspiranteComponent, {
+      data: { aspiranteIndividual: aspirante }, 
+      height: '800px',
+      width: '1000px',
+      
+    });
+
+    dialogo1.afterClosed().subscribe(art => {
+      if (art != undefined)
+        this.agregar(art);
+    });
+  }
+
+  agregar(art: any) {
+    this.listausuariosAspirantes.push();
+    //this.tabla1.renderRows();
+    this.dataSource.renderRows();
+  }
+  
 
 
   getUsuarios(){
@@ -25,6 +57,18 @@ export class AspirantesAceptadosComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    
+    setTimeout(()=>{
+      this.usuariosAspirantes();
+      setTimeout(()=>{
+        this.dataSource = new MatTableDataSource(this.listausuariosAspirantes);
+        //this.dataSource.paginator=this.paginator
+  
+      }, 300);
+
+    }, 500);
+
     this.http.get('https://agencialaboralproyecto.pythonanywhere.com/api/usuarios/').subscribe((doc:any)=>{
       this.usuarios=doc
       
@@ -35,11 +79,36 @@ export class AspirantesAceptadosComponent implements OnInit {
     this.http.get('https://agencialaboralproyecto.pythonanywhere.com/api/aspirantes/').subscribe((doc:any)=>{
       this.aspirantes=doc
       
-      console.log(this.usuarios)
+      console.log(this.aspirantes)
 
     })
 
   }
+
+
+  usuariosAspirantes(){
+    for( let user of this.usuarios){
+      for(let asp of this.aspirantes){
+        if(user.idusuario==asp.usuario_idusuario){
+          if (user.rol_idrol==2 && asp.estadoaspirantes_idestadoaspirantes == 1){
+            //this.listausuariosAspirantes.push(user)
+            this.listausuariosAspirantes.indexOf(user) === -1 ? this.listausuariosAspirantes.push(user):
+            console.log("This item already exists");
+            console.log(this.listausuariosAspirantes)
+          
+          
+      }
+        }
+      }
+    } 
+  }
+
+  filtrar(event: Event) {
+    const filtro = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filtro.trim().toLowerCase();
+  } 
+
+
   mostrar(id: any){
     console.log("NUEVOID",id)
     this.idAspirante=id,
--- src/app/components/administrador/aspirantes-nuevos/aspirantes-nuevos.component.ts
+++ src/app/components/administrador/aspirantes-nuevos/aspirantes-nuevos.component.ts
@@ -1,6 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AspiranteService } from 'src/app/servicios/aspirante.service';
+import { VistaPerfilAspiranteComponent } from '../vista-perfil-aspirante/vista-perfil-aspirante.component';
 
 @Component({
   selector: 'app-aspirantes-nuevos',
@@ -9,14 +16,47 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AspirantesNuevosComponent implements OnInit {
 
-  
+  aspirantes: any[]=[];
   aspirantesNuevos: any[]=[];
   representantes: any[]=[];
   usuarios: any[]=[];
+  listausuariosAspirantes: any[]=[];
+  listausuariosAspirantes2: any[]=[];
+  columnas: string[] = ['nombreusuario','nombre', 'apellido','nodocumento', 'correo','boton', 'aceptar'];
+  dataSource:any; 
 
-  constructor( private http: HttpClient, private rutaActiva: ActivatedRoute) {
+  suscription: Subscription; 
+
+
+  constructor(
+    private aspiranteService: AspiranteService, 
+    private fb: FormBuilder, 
+    private http: HttpClient, 
+    private rutaActiva: ActivatedRoute,
+    public dialog: MatDialog) {
     this.getRepresentantes()
     this.getAspirantesNuevos();
+  }
+
+
+  abrirDialogo(aspirante: any) {
+    const dialogo1 = this.dialog.open(VistaPerfilAspiranteComponent, {
+      data: { aspiranteIndividual: aspirante }, 
+      height: '800px',
+      width: '1000px',
+      
+    });
+
+    dialogo1.afterClosed().subscribe(art => {
+      if (art != undefined)
+        this.agregar(art);
+    });
+  }
+
+  agregar(art: any) {
+    this.listausuariosAspirantes.push();
+    //this.tabla1.renderRows();
+    this.dataSource.renderRows();
   }
   
   getRepresentantes(){
@@ -30,7 +70,7 @@ export class AspirantesNuevosComponent implements OnInit {
   getAspirantesNuevos(){
     this.http.get('https://agencialaboralproyecto.pythonanywhere.com/api/usuarios/').subscribe((doc:any)=>{
       this.usuarios =doc
-      console.log(this.usuarios)
+      console.log("USUARIOS",this.usuarios)
       /*doc.forEach(user => {
         this.representantes.forEach(rep=>{
           console.log("usuario",user.idusuario)
@@ -50,8 +90,149 @@ export class AspirantesNuevosComponent implements OnInit {
     })
   }
 
-  ngOnInit(): void {
+  usuariosAspirantes(){
+    for( let user of this.usuarios){
+      for(let asp of this.aspirantes){
+        if(user.idusuario==asp.usuario_idusuario){
+          if (user.rol_idrol==2 && asp.estadoaspirantes_idestadoaspirantes == 2){
+            //this.listausuariosAspirantes.push(user)
+            this.listausuariosAspirantes.indexOf(user) === -1 ? this.listausuariosAspirantes.push(user):
+            console.log("This item already exists");
+            console.log("usuarios que quedan",this.listausuariosAspirantes)
+          
+          
+        }
+          if(user.rol_idrol==2 && asp.estadoaspirantes_idestadoaspirantes == 1){
+            const index= this.listausuariosAspirantes.indexOf(user);
+            this.listausuariosAspirantes.indexOf(user) > -1 ? this.listausuariosAspirantes.splice(index,1):
+            console.log("Borrado prov");
+            console.log("usuarios que quedan",this.listausuariosAspirantes)
+          }
+        }
+      }
+    } 
+  }
 
+
+  usuariosAspirantesAceptados(){
+    this.listausuariosAspirantes=[]
+    for( let user of this.usuarios){
+      for(let asp of this.aspirantes){
+        if(user.idusuario==asp.usuario_idusuario){
+          if (user.rol_idrol==2 && asp.estadoaspirantes_idestadoaspirantes == 2){
+            //this.listausuariosAspirantes.push(user)
+            this.listausuariosAspirantes.indexOf(user) === -1 ? this.listausuariosAspirantes.push(user):
+            console.log("This item already exists");
+            console.log("usuarios que quedan",this.listausuariosAspirantes)
+          
+          
+      }
+        }
+      }
+    } 
+  }
+
+  filtrar(event: Event) {
+    const filtro = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filtro.trim().toLowerCase();
+  } 
+
+  ngOnInit(): void {
+    this.http.get('https://agencialaboralproyecto.pythonanywhere.com/api/aspirantes/').subscribe((doc:any)=>{
+      this.aspirantes=doc
+      
+      console.log(this.aspirantes)
+
+    })
+
+    setTimeout(()=>{
+      this.usuariosAspirantes();
+      setTimeout(()=>{
+        this.dataSource = new MatTableDataSource(this.listausuariosAspirantes);
+  
+      }, 300);
+
+    }, 500);
+
+    this.suscription = this.aspiranteService.refresh$.subscribe(()=>{
+      this.ngOnInit()
+
+
+
+      /*this.http.get('https://agencialaboralproyecto.pythonanywhere.com/api/aspirantes/').subscribe((doc:any)=>{
+      this.aspirantes=doc
+      
+      console.log("ASPIRANTES",this.aspirantes)
+
+      })
+      this.getAspirantesNuevos()
+
+      
+      setTimeout(()=>{
+        this.usuariosAspirantes();
+        console.log("a")
+
+        setTimeout(()=>{
+          
+          //let data= new MatTableDataSource(this.listausuariosAspirantes);
+          //this.dataSource.data = this.listausuariosAspirantes
+          this.ngOnInit()
+    
+        }, 300);
+  
+      }, 300);*/
+
+    })
+
+  }
+
+  ngOnDestroy():void{
+    this.suscription.unsubscribe();
+    console.log('Observable cerrado');
+  }
+
+  miFormulario: FormGroup = this.fb.group({
+    estadoaspirantes_idestadoaspirantes: 2,
+    
+
+  })
+
+
+
+  aceptarAspirante(id : any){
+
+    if(confirm("¿Está seguro de que desea aceptar este aspirante?")){
+      for(let asp of this.aspirantes){
+        if(asp.usuario_idusuario==id){
+          let headers = new HttpHeaders();
+          let formData= new FormData();
+          formData.append("estadoaspirantes_idestadoaspirantes", '1')
+          console.log(asp.idaspirante)
+          headers.append('Content-Type', 'application/json');
+  
+          
+          this.aspiranteService.patchAspirante(asp.idaspirante,formData).subscribe(data=>{
+            console.log("Datos del post",data)
+            //this.ngOnInit()
+            //this.listausuariosAspirantes2 = this.listausuariosAspirantes
+            alert('ASPIRANTE ACEPTADO');
+            //this.miFormulario.reset();
+          });
+  
+          
+  
+  
+          /*this.http.patch<any>("https://agencialaboralproyecto.pythonanywhere.com/api/aspirantes/"+asp.idaspirante,formData,{headers: headers}).subscribe(
+            resp => console.log(resp),
+            err => console.log(err)
+              );*/
+              
+          
+  
+        }
+      }
+    }
+    
   }
  
 
